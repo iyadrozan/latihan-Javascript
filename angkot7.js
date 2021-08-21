03edc8cba758bdc753935a81af8623342650a306
iyadrozan <iyadrozan26@gmail.com>
2021-08-21 09:53:48 +0800

    Penambahan materi dari pak sandhika galih

---
+++ angkot7.js
@@ -0,0 +1,11 @@
+var Angkot = parseInt(prompt("Masukan Nomor angkot yang ingin kamu pesan: "));
+
+if (Angkot === 1) {
+  alert("Anda memesan angkot nomor 1");
+} else if (Angkot === 2) {
+  alert("Anda memasukan angkot nomor 2");
+} else if (Angkot === 3) {
+  alert("Anda memasukan angkot nomor 3");
+} else {
+  alert("Nomor angkot yang anda masukan tidak terdaftar");
+}
